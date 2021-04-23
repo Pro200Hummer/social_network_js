6476f6510583dd334f26267755539e6b945281dc
Pro200Hummer <nikolaj4ik.pasha@yandex.ru>
2021-04-23 17:34:39 +0300

    Created initial state to users-reducer and action creators to following users.

--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -1,8 +1,60 @@
+import userAvatar from "../resources/images/user-avatar.png"
+
+const FOLLOW = "FOLLOW"
+const UNFOLLOW = "UNFOLLOW"
 
 let initialState = {
-
+    users: [
+        {
+            id: 1,
+            firstName: "Pavel",
+            lastName: "Nickolaichik",
+            location: {
+                country: "Belarus",
+                city: "Minsk"
+            },
+            status: "I'm looking for a job right now",
+            avatar: userAvatar,
+            followed: true
+        },
+        {
+            id: 2,
+            firstName: "Natali",
+            lastName: "Nickolaichik",
+            location: {
+                country: "Belarus",
+                city: "Minsk"
+            },
+            status: "I'm zaebalas uzhe",
+            avatar: userAvatar,
+            followed: true
+        },
+        {
+            id: 3,
+            firstName: "Monya",
+            lastName: "Nickolaichik",
+            location: {
+                country: "Belarus",
+                city: "Minsk"
+            },
+            status: "meoooow",
+            avatar: userAvatar,
+            followed: false
+        },
+        {
+            id: 4,
+            firstName: "Frosya",
+            lastName: "Nickolaichik",
+            location: {
+                country: "Belarus",
+                city: "Minsk"
+            },
+            status: "I will demolish the whole apartment",
+            avatar: userAvatar,
+            followed: false
+        },
+    ]
 }
-
 
 const usersReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -11,5 +63,8 @@ const usersReducer = (state = initialState, action) => {
             return state
     }
 }
+
+const followAC = () => ({type: FOLLOW})
+const unfollowAC = () => ({type: UNFOLLOW})
 
 export default usersReducer;
