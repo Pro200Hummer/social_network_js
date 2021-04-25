4f4096e9b6b8d0e4814481fe4b0501a4baa8fcba
Pro200Hummer <nikolaj4ik.pasha@yandex.ru>
2021-04-25 18:39:54 +0300

    Component User has been rewritten from functional to class.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,7 +3,52 @@ import us from './Users.module.css'
 import axios from "axios";
 import avatar from "../../resources/images/user-avatar.png"
 
-const Users = (props) => {
+class Users extends React.Component {
+    constructor(props){
+        super(props);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(r => {
+            console.log(r)
+            props.setUsers(r.data.items.map(i => {
+                if(i.photos.small === null){
+                    return {...i, photos:{...i.photos, small: avatar}}
+                }
+                return i
+            }))
+        })
+    }
+    render(){
+        return <div>
+            {
+                this.props.users.map(u => {
+                        const followClickHandler = () => {
+                            this.props.follow(u.id)
+                        }
+                        const unFollowClickHandler = () => {
+                            this.props.unfollow(u.id)
+                        }
+
+                        const button = u.followed ?
+                            <button onClick={followClickHandler}>Follow</button> :
+                            <button onClick={unFollowClickHandler}>Unfollow</button>
+                        return <div key={u.id}>
+                            <div><img src={u.photos.small} className={us.avatar} alt="#"/></div>
+                            <div>
+                                {button}
+                            </div>
+                            <div>
+                                <div>{u.name}</div>
+                                <div>{u.status}</div>
+                            </div>
+                        </div>
+                    }
+                )
+            }
+        </div>
+    }
+}
+
+
+/*const Users = (props) => {
     if (props.users.length === 0) {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(r => {
             console.log(r)
@@ -43,6 +88,8 @@ const Users = (props) => {
             )
         }
     </div>
-}
+}*/
+
+
 
 export default Users
